f2fef1dcd60db9ce68b566033ae1499de3f5026d
haris_delalic <delalicharis@gmail.com>
2025-04-11 19:30:02 +0200

    create ticket form

---
+++ src/components/TicketForm.js
@@ -0,0 +1,39 @@
+import React, {useState} from "react";
+import '../styles.css';
+
+export default function TicketForm() {
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [priority, setPriority] = useState('1')
+
+    const priorityLabel = {
+        1: 'Low',
+        2: 'Medium',
+        3: 'High'
+    }
+
+    const clearForm = () => {
+        setTitle('');
+        setDescription('');
+        setPriority('1');
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        clearForm();
+    }
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label className="label">Title</label>
+                <input type="text" className="form-input" value={title} onChange={(e) => setTitle(e.target.value)}/>
+            </div>
+
+            <div>
+                <label className="label">Description</label>
+                <textarea className="form-input" value={description} onChange={(e) => setDescription(e.target.value)}/>
+            </div>
+        </form>
+    )
+}
