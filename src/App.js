2aaff4d772687bae36d2b03332a027ea1392a416
haris_delalic <delalicharis@gmail.com>
2025-04-12 13:47:16 +0200

    implement logic for adding new ticket

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import './styles.css'
 import TicketForm from "./components/TicketForm";
+import ticketReducer from "./reducers/ticketReducer";
+import {useReducer} from "react";
 
 function App() {
+    const initialState = { tickets: [] }
+    const [state, dispatch] = useReducer(ticketReducer, initialState)
+
     return (
         <div className="container">
           <h1>Bug Blaster</h1>
-            <TicketForm></TicketForm>
+            <TicketForm dispatch={dispatch}></TicketForm>
         </div>
     );
 }
